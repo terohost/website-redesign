3735e0d7f2f2e91a12546f91d187cf56137cd799
Arinji2 <87296817+Arinji2@users.noreply.github.com>
2022-12-11 09:30:06 +0530

    Change Hero

--- src/pages/hero.tsx
+++ src/pages/hero.tsx
@@ -1,17 +1,27 @@
 import { TypeAnimation } from "react-type-animation";
-import bg from "../assets/bg.jpg";
+
+import earth from "../assets/planet.mp4";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { IconProp } from "@fortawesome/fontawesome-svg-core";
+import { faCartPlus } from "@fortawesome/fontawesome-free-solid";
 
 function Hero() {
   return (
-    <div className="flex flex-col md:flex-row items-center justify-center bg-black w-full h-screen gap-10 pt-10">
-      <img
-        className="h-screen  md:w-full opacity-30 absolute md:block hidden"
-        src={bg}
-      ></img>
-      <img
+    <div className="flex flex-col md:flex-row items-center justify-center bg-black w-full h-screen gap-10 ">
+      <video
+        className="h-screen object-cover opacity-30 md:w-full  absolute md:block hidden"
+        src={earth}
+        muted
+        autoPlay
+        loop
+      ></video>
+      <video
         className="h-screen object-cover  md:w-full opacity-30 absolute md:hidden block"
-        src={bg}
-      ></img>
+        src={earth}
+        muted
+        autoPlay
+        loop
+      ></video>
       <div className="z-10 flex flex-col items-center justify-center gap-10 text-center">
         <h1 className="font-extrabold text-3xl md:text-4xl text-white">
           All Of Your Hosting Needs Done
@@ -41,9 +51,14 @@ function Hero() {
           to Game Servers to VPN's at a cheap price without any compromise in
           quality, all in one place.
         </p>
-        <p className="mt-10 border-2 border-yellow-600 hover:border-yellow-500 bg-yellow-600 hover:bg-yellow-500 text-white  text-xl p-2 shadow-md shadow-black hover:cursor-pointer transition-all ease-in-out duration-300 rounded-lg">
-          Start Hosting!
-        </p>
+        <div className="flex flex-row items-center w-full justify-center gap-10 mt-20">
+          <p className="mt-10 border-2 border-yellow-600  bg-yellow-600 hover:mb-10 absolute ml-40 hover:bg-slate-600 hover:border-slate-600 hover:border-2 pl-5 pr-5 text-white  text-xl p-2 shadow-md shadow-black hover:cursor-pointer transition-all ease-in-out duration-300 rounded-lg">
+            Learn More
+          </p>
+          <p className="mt-10 border-2 border-yellow-600  bg-yellow-600 hover:mb-10 absolute mr-40 hover:bg-slate-600 hover:border-slate-600 hover:border-2 pl-5 pr-5 text-white  text-xl p-2 shadow-md shadow-black hover:cursor-pointer transition-all ease-in-out duration-300 rounded-lg">
+            Buy Now <FontAwesomeIcon icon={faCartPlus as IconProp} />
+          </p>
+        </div>
       </div>
     </div>
   );
